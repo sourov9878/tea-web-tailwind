0ab4b05af5dda5ed6804d8df198addd57f52b4a7
sourov9878 <forhadhossan047@gmail.com>
2025-06-17 09:50:48 +0600

    test

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // ...
+  presets: [
+    require('@acmecorp/base-tailwind-config')
+  ],
+
+  // Project-specific customizations
+  theme: {
+    //...
+  },
+}
